bc9ae2db007e450c2d38cb6a85f7d472275e7e94
Olivier Banville <oli.banville@gmail.com>
2023-11-25 17:58:05 -0500

    remove db check

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
-# set -e
-
-# Wait for the database to be ready before migrating
-if [ ! -z "$DB_HOST" ]; then
-  >&2 echo "Waiting for database connection on $DB_HOST:$DB_PORT"
-  until nc -z $DB_HOST $DB_PORT
-  do
-    >&2 echo "Waiting for database connection..."
-    sleep 1
-  done
-  >&2 echo "Waiting for database connection... Done"
-fi
 
 if [ $DJANGO_MANAGE_MIGRATE = 'on' ]; then
   python manage.py collectstatic --noinput
